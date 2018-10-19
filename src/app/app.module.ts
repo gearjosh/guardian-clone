4d2b587fc0783f32c1949eea5b64fdf2df2e7f53
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-19 09:43:56 -0700

    add headline component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadlinesComponent
   ],
   imports: [
     BrowserModule
