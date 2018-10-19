c77796500da179e184cd4e54ceb516344d6495da
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-19 13:29:49 -0700

    configure mock import, export

--- src/app/headlines/headlines.component.ts
+++ src/app/headlines/headlines.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Story from '../models/story.model';
+import {STORIES} from '../mocks/stories';
 
 @Component({
   selector: 'app-headlines',
@@ -7,8 +8,11 @@ import Story from '../models/story.model';
   styleUrls: ['./headlines.component.scss']
 })
 export class HeadlinesComponent implements OnInit {
+  stories = STORIES;
 
-  constructor(public story1: Story, public story2: Story, public story3: Story, public story4: Story, public story5: Story, public story6: Story, public story7: Story, public story8: Story, public story9: Story, public story10: Story, public story11: Story, public story12: Story, public story13: Story, public story14: Story, public story15: Story, public story16: Story, public story17: Story) { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
