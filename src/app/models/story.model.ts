69337bef344aa7249639c3eef3cef19d0e4efcfa
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-19 13:49:29 -0700

    add topic sorter

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -1,7 +1,30 @@
+import { STORIES } from '../mocks/stories';
+
 export default class Story {
 
-  constructor(public headline: string, public lead: string, public body: string, public byline: string, public topics: string[], public priority: number = 3) {
+  constructor(public headline: string, public lead: string, public body: string, public byline: string, public topics: string[], public priority: number = 3) { };
 
-  }
+  stories = STORIES;
+
+  prioritySorter(stories) {
+    let p1Array: Story[];
+    let p2Array: Story[];
+    let p3Array: Story[];
+    stories.forEach((Story) => {
+      let outputArray = [p1Array, p2Array, p3Array];
+      if (Story.priority === 1) {
+        p1Array.push(Story);
+      } else if (Story.priority === 2) {
+        p2Array.push(Story);
+      } else {
+        p3Array.push(Story);
+      }
+      return outputArray;
+    });
+  };
+
+  topicSorter(story, stories) {
+    
+  };
 
 }
