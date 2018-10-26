0a970f0558892907aa7388f0cd6e1efcbca09ea5
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-26 16:19:21 -0700

    remove unused code

--- src/app/models/story.model.ts
+++ src/app/models/story.model.ts
@@ -2,23 +2,6 @@ export class Story {
 
   constructor(public headline: string, public lead: string, public body: string, public byline: string, public topics: string[], public priority: number = 3) {};
 
-  // prioritySorter(stories) {
-  //   let p1Array: Story[];
-  //   let p2Array: Story[];
-  //   let p3Array: Story[];
-  //   stories.forEach((Story) => {
-  //     let outputArray = [p1Array, p2Array, p3Array];
-  //     if (Story.priority === 1) {
-  //       p1Array.push(Story);
-  //     } else if (Story.priority === 2) {
-  //       p2Array.push(Story);
-  //     } else {
-  //       p3Array.push(Story);
-  //     }
-  //     return outputArray;
-  //   });
-  // };
-
   findRelated(inputStory: Story, stories: Story[]) {
     const topicsToMatch: string[] = inputStory.topics;
     let outputArray: Story[];
@@ -35,37 +18,4 @@ export class Story {
     });
     return outputArray;
   };
-
-  // compareTopStories(a, b) {
-  //   const topicsA = a.topics.length;
-  //   const topicsB = b.topics.length;
-  //
-  //   let comparison: number = 0;
-  //
-  //   if (topicsA > topicsB) {
-  //     comparison = 1;
-  //   } else if (topicsB > topicsA) {
-  //     comparison = -1
-  //   }
-  //   return comparison;
-  // };
-  //
-  // findTopStory(stories: Story[]) {
-  //   let topStory: Story;
-  //   let topStoryPossibilities: Story[] = [];
-  //   // let topicScores: number[];
-  //
-  //   stories.forEach((story: Story) => {
-  //     if (story.priority === 1) {
-  //       topStoryPossibilities.push(story);
-  //     } else {
-  //       return;
-  //     }
-  //   });
-  //
-  //   topStoryPossibilities.sort(this.compareTopStories);
-  //
-  //   return topStory;
-  // };
-
 }
