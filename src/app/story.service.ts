ade7ba45f391e6645413e274d63828d9e81d332d
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-10-26 10:50:43 -0700

    add story service

---
+++ src/app/story.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Story } from './models/story.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoryService {
+
+  constructor() { }
+
+  getStories() {
+    // return stories;
+  }
+}
